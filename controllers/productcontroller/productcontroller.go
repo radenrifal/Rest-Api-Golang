90bc6f84f2f605eff1050fe79852eaa80ed381c7
radenrifal <radenrifalardiansyah@gmail.com>
2022-12-14 01:42:50 +0700

    Service Update API

--- controllers/productcontroller/productcontroller.go
+++ controllers/productcontroller/productcontroller.go
@@ -1,11 +1,15 @@
 package productcontroller
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/radenrifal/Rest-Api-Golang/helper"
 	"github.com/radenrifal/Rest-Api-Golang/models"
+	"gorm.io/gorm"
 )
 
 var ResponseJson = helper.ResponseJson
@@ -24,15 +28,68 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 func Show(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.ParseInt(vars["id"], 10, 64)
+	if err != nil {
+		ResponseError(w, http.StatusBadRequest, err.Error())
+	}
 
+	var product models.Product
+	if err := models.DB.First(&product, id).Error; err != nil {
+		switch err {
+		case gorm.ErrRecordNotFound:
+			ResponseError(w, http.StatusNotFound, "Product Not Found")
+			return
+		default:
+			ResponseError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+	}
+
+	ResponseJson(w, http.StatusOK, product)
 }
 
 func Create(w http.ResponseWriter, r *http.Request) {
+	var product models.Product
 
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&product); err != nil {
+		ResponseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	defer r.Body.Close()
+
+	if err := models.DB.Create(&product).Error; err != nil {
+		ResponseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	ResponseJson(w, http.StatusCreated, product)
 }
 
 func Update(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.ParseInt(vars["id"], 10, 64)
+	if err != nil {
+		ResponseError(w, http.StatusBadRequest, err.Error())
+	}
 
+	var product models.Product
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&product); err != nil {
+		ResponseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	defer r.Body.Close()
+
+	if models.DB.Where("id = ?", id).Updates(&product).RowsAffected == 0 {
+		ResponseError(w, http.StatusBadRequest, "Not Update Product")
+		return
+	}
+
+	product.Id = id
+	ResponseJson(w, http.StatusOK, product)
 }
 
 func Delete(w http.ResponseWriter, r *http.Request) {
